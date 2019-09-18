a5fada745fb0e2f33346e51ba2a0f841757203b0
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-09-18 16:34:37 +0000

    feat: browser only, smaller bundle
    BREAKING CHANGE: no more Node.js support

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-require('core-js/fn/array/includes');
-require('core-js/fn/object/values');
-var DOMException = require('domexception');
 var Typeson = require('typeson');
 var structuredCloningThrowing = require('typeson-registry/dist/presets/structured-cloning-throwing');
 
 // http://stackoverflow.com/a/33268326/786644 - works in browser, worker, and Node.js
-var globalVar = typeof window !== 'undefined' ? window : 
-   typeof WorkerGlobalScope !== 'undefined' ? self :
-   typeof global !== 'undefined' ? global :
-   Function('return this;')();
-
-if (!globalVar.DOMException) {
-    globalVar.DOMException = DOMException;
-}
-
 var TSON = new Typeson().register(structuredCloningThrowing);
 
 function realisticStructuredClone(obj) {
